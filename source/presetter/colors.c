bdc48cd4305714400ff44ec9334a75a1c3434cec
hiamani <hiamani@pm.me>
2026-03-17 22:02:04 -0400

    fix: presetter_resolve_color alpha impl

--- source/presetter/colors.c
+++ source/presetter/colors.c
@@ -9,11 +9,12 @@ void presetter_hex_to_rgba(t_jrgba *color, const char *hex, double alpha) {
     jrgba_set(color, r / 255.0, g / 255.0, b / 255.0, alpha);
 }
 
-void presetter_resolve_color(p_color pc, t_jrgba *color) {
+void presetter_resolve_color(p_color pc, t_jrgba *color, double alpha) {
     if (pc.type == COLOR_HEX) {
-        presetter_hex_to_rgba(color, pc.value, 1.0);
+        presetter_hex_to_rgba(color, pc.value, alpha);
     } else {
         t_jrgba off;
         jcolor_getcolor(gensym(pc.value), color, &off);
+        color->alpha = alpha;
     }
 }
