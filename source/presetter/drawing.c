d7d72e739061cef1f9a6f179a7d7b019e808f2f8
hiamani <hiamani@pm.me>
2026-03-24 14:39:07 -0400

    perf: Remove unnecessary jgraphics_text_measure from presetter_trim_text_right

--- source/presetter/drawing.c
+++ source/presetter/drawing.c
@@ -24,8 +24,6 @@ static void presetter_trim_text_right(t_jgraphics *g, const char *text, double m
         snprintf_zero(out, outsize, "%s", text);
 
         char with_ellipsis[1048];
-        snprintf_zero(with_ellipsis, sizeof(with_ellipsis), "%s...", out);
-        jgraphics_text_measure(g, with_ellipsis, &text_width, &text_height);
 
         while (text_width > max_width && strlen(out) > 0) {
             out[strlen(out) - 1] = '\0';
